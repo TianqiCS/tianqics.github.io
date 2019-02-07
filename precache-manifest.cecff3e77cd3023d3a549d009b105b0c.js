fe07634fb444c010ff57198e4f9cf3d0654971b0
Tianqi Wang <twang2@ualberta.ca>
2019-02-07 13:25:30 -0700

    some changes

---
+++ precache-manifest.cecff3e77cd3023d3a549d009b105b0c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "0c30180519820867c45c",
+    "url": "/static/js/main.0c301805.chunk.js"
+  },
+  {
+    "revision": "f2f25b858e52d1354972",
+    "url": "/static/js/1.f2f25b85.chunk.js"
+  },
+  {
+    "revision": "0c30180519820867c45c",
+    "url": "/static/css/main.3bdea2d4.chunk.css"
+  },
+  {
+    "revision": "b421176aff8194a53cce0c0b4fdfdb6d",
+    "url": "/index.html"
+  }
+];
